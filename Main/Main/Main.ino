f000d430d72fbc65dfaf548d35af8e8002d71d87
albert810 <ahwildeboer@gmail.com>
2017-11-16 13:44:51 +0100

    Opstart

---
+++ Main/Main/Main.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Main.ino
+ Created:	11/16/2017 1:42:53 PM
+ Author:	Albert
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
